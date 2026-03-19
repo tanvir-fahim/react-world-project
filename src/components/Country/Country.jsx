843c49fd4cd174a55609a934935f43069abb76af
Tanvir-Fahim <mt009.fahim@gmail.com>
2026-03-20 00:28:41 +0600

    Project finished

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "./Country.css"
 
-const Country = ({country}) => {
-    console.log(country);
+const Country = ({country, handleVisitedCountries, handleVisitedFlags}) => {
+    // console.log(country);
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        // if(visited){
+        //     setVisited(false);
+        // }else{
+        //     setVisited(true);
+        // }
+
+        // setVisited(visited ? false : true);
+
+        setVisited(!visited);
+        handleVisitedCountries(country);
+    }
     return (
-        <div>
-            <img src={country.flags.flags.png} alt={country.flags.flags.alt}/>
+        <div className="country">
+            <img className='takeSpace' src={country?.flags?.flags?.png} alt={country.flags.flags.alt}/>
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
+            <p>Area: {country.area.area}
+                {country.area.area > 300000 ? "  Big Country": "  Small Country"}
+            </p>
+            <button className={`${visited ? "country-visited" : "country-notVisited"}`} onClick={handleVisited}>
+                {visited ? "Visited" : "Not Visited"}
+            </button>
+            <button className='btn' onClick = {() => {handleVisitedFlags(country?.flags?.flags?.png)}}>Add country flag</button>
         </div>
     );
 };
--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,16 +1,49 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../Country/Country';
+import "./Countries.css"
 
 const Countries = ({countriesPromise}) => {
+    const [visitedCountries, setVisitedCountries] = useState([]);
+    const [visitedFlags, setVisitedFlags] = useState([]);
+
+    const handleVisitedCountries = (country) => {
+        console.log("Handle visited country cliked", country);
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+    }
+
+    const handleVisitedFlags = (flag) => {
+        // console.log("Flags need to add.", flag);
+        const newVisitedFlags = [...visitedFlags, flag];
+        setVisitedFlags(newVisitedFlags);
+    }
+
+
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
-    console.log(countries);
+    // console.log(countries);
     return (
         <div>
             <h1>In the countries: {countries.length}</h1>
-            {
-                countries.map(country => <Country country = {country}></Country>)
-            }
+            <h3>Total Country Visited: {visitedCountries.length}</h3>
+            <h4>Country Flags: {visitedFlags.length}</h4>
+            <ol>
+                {visitedCountries.map(country => <li key={country.ccn3.ccn3}> {country.name.common} </li>)}
+            </ol>
+            <div className='visited-flag-container'>
+                {
+                    visitedFlags.map((flag, index) => {
+                      return <img key={index} src={flag} alt="" />
+                    })
+                }
+            </div>
+            <div className='countries'>
+                {
+                    countries.map(country => <Country country = {country} key={country.ccn3.ccn3} handleVisitedCountries = {handleVisitedCountries}
+                    handleVisitedFlags = {handleVisitedFlags}
+                    ></Country>)
+                }
+            </div>
         </div>
     );
 };
