fe3ac6f8018c2f04812be5fcffe63a1fe6caa44e
Tanvir-Fahim <mt009.fahim@gmail.com>
2026-03-19 16:04:11 +0600

    Project initialized

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Countries from './components/countries/Countries'
+
+function App() {
+
+  return (
+    <>
+      <h1>React world on the go...</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
